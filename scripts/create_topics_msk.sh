37cb643ddc119961535e1877f1332b56c8adf536
Todd Hunter <todd.hunter@gmail.com>
2022-07-02 04:12:29 +0000

    Adding opportunies avro

--- scripts/create_topics_msk.sh
+++ scripts/create_topics_msk.sh
@@ -10,12 +10,12 @@ fi
 
 PROPERTIES_FILE=kafka_tmp.properties
 
-cat <<EOF
+cat <<EOF > $PROPERTIES_FILE
 security.protocol=SASL_SSL
 sasl.mechanism=AWS_MSK_IAM
 sasl.jaas.config=software.amazon.msk.auth.iam.IAMLoginModule required;
 sasl.client.callback.handler.class=software.amazon.msk.auth.iam.IAMClientCallbackHandler
-EOF > $PROPERTIES_FILE
+EOF
 
 kafka-topics --bootstrap-server $BOOTSTRAP --command-config $PROPERTIES_FILE --create --topic trades --partitions 6 --replication-factor 3
 kafka-topics --bootstrap-server $BOOTSTRAP --command-config $PROPERTIES_FILE --create --topic positions --partitions 6 --replication-factor 3
@@ -24,3 +24,6 @@ rm $PROPERTIES_FILE
 
 kafka-topics --describe --bootstrap-server $BOOTSTRAP --topic trades
 kafka-topics --describe --bootstrap-server $BOOTSTRAP --topic positions
+
+kafka-topics --bootstrap-server $BOOTSTRAP --command-config $PROPERTIES_FILE --create --topic trades.stream.buy  --partitions 6 --replication-factor 3
+kafka-topics --bootstrap-server $BOOTSTRAP --command-config $PROPERTIES_FILE --create --topic trades.stream.sell  --partitions 6 --replication-factor 3
